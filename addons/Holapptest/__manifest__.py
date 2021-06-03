c9eb7806c0e1a14afbdf180e0c9fbe242ec2fcf3
Tom <cheng@dxs.ai>
2021-06-03 02:13:36 -0500

    Docker compose and volumes

---
+++ addons/Holapptest/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Air Flow: Custom Invoice PDF",
+    "summary": """Customizing PDF with updated information""",
+    "description": """
+        Adding specifications:
+    """,
+    "author": "HPO",
+    "version": "1.0",
+    "category": "Custom",
+    "depends": ["base"],
+    "data": [],
+    "demo": []
+}
